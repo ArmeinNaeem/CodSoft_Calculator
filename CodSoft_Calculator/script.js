397cd04f24c206797d94b0ec54da3bca7210a380
ArmeinNaeem <armeennaeem15@gmail.com>
2024-07-13 23:27:45 +0500

    Add files via upload

---
+++ CodSoft_Calculator/script.js
@@ -0,0 +1,15 @@
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+function calculate() {
+    try {
+        document.getElementById("result").value = eval(document.getElementById("result").value);
+    } catch (err) {
+        document.getElementById("result").value = "Error";
+    }
+}
